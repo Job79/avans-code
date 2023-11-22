eb613d11ca28e359c565d29230c7582ce116716b
Job79 <job@plabble.org>
2023-11-22 15:26:25 +0100

    feat: add mongo

--- apps/avans-code-api/src/app/app.module.ts
+++ apps/avans-code-api/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import {UsersModule} from "@avans-code/backend/users";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(process.env['MONGODB_CONNECTION_STRING'] ?? 'mongodb://localhost/avans-code'),
+    UsersModule
+  ],
   controllers: [],
   providers: [AppService],
 })
