95cb1947db7d6e92ce5abadd27ed6d5aa4aba4ed
Job79 <job@plabble.org>
2023-11-25 19:37:48 +0100

    refactor: move about page to app module

--- libs/ui/features/src/lib/about/about.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'avans-code-about',
-  templateUrl: './about.component.html',
-  styles: [],
-})
-export class AboutComponent { }
