7253bbf24d8dfb4069d2f4e64c2f6802e956037f
Job79 <job@plabble.org>
2023-12-08 20:02:03 +0100

    fix: import config module

--- apps/avans-code-api/src/app/app.module.ts
+++ apps/avans-code-api/src/app/app.module.ts
@@ -5,9 +5,11 @@ import {AuthModule} from "@avans-code/backend/auth";
 import {AssignmentsModule} from "@avans-code/backend/assignments";
 import {TagsModule} from "@avans-code/backend/tags";
 import {RecommendationsModule} from "@avans-code/backend/recommendations";
+import {ConfigModule} from "@nestjs/config"
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env['MONGODB_CONNECTION_STRING'] ?? 'mongodb://localhost/avans-code'),
     UsersModule,
     AuthModule,
