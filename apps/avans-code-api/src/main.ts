a1954d8e1d21f90b1b642265b5a8a1c23c3dd861
Job79 <job@plabble.org>
2023-12-10 12:24:39 +0100

    fix: enable cors in backend

--- apps/avans-code-api/src/main.ts
+++ apps/avans-code-api/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
