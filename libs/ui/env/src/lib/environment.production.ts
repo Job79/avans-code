06f5e897a5c4add846a3fa92b9411288a179c279
Job79 <job@plabble.org>
2023-12-10 12:28:47 +0100

    chore: update url in production environment

--- libs/ui/env/src/lib/environment.production.ts
+++ libs/ui/env/src/lib/environment.production.ts
@@ -3,6 +3,6 @@ import {IEnvironment} from "./environment.interface";
 export const Environment: IEnvironment = {
   production: true,
   api: {
-    url: 'http://localhost:3000/api'
+    url: 'https://avans-code.azurewebsites.net/api'
   }
 };
