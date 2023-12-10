36bbe8eb75bdc29db3440310a6fb9a748950afd3
Job79 <job@plabble.org>
2023-12-10 12:22:28 +0100

    feat: add env project

---
+++ libs/ui/env/src/lib/environment.production.ts
@@ -0,0 +1,8 @@
+import {IEnvironment} from "./environment.interface";
+
+export const Environment: IEnvironment = {
+  production: true,
+  api: {
+    url: 'http://localhost:3000/api'
+  }
+};
---
+++ libs/ui/env/src/lib/environment.ts
@@ -0,0 +1,8 @@
+import {IEnvironment} from "./environment.interface";
+
+export const Environment: IEnvironment = {
+  production: false,
+  api: {
+    url: 'http://localhost:3000/api'
+  }
+};
